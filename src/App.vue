366a7451c34ba91ec282a3d24add6d1fbfad7e72
Henry Colque Larrea <henry.colque@asfi.gov.bo>
2025-10-21 16:13:39 -0400

    Ejemplo 03-Directivas

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container mt-3">
+
+    <Header />
+    <Formulario />
+  </div>
+</template>
+
+<script>
+import Formulario from './components/Formulario.vue';
+import Header from './components/Header.vue';
+
+export default {
+  components: {
+    Header,
+    Formulario
+  }
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="row">
+    <div class="col-12 col-md-3">
+      <h1>Formularios</h1>
+    </div>
+    <div class="col-12 col-md-6">
+      <img :src="url" width="150">
+    </div>
+  </div>
+  <hr>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    url: "https://v2.vuejs.org/images/logo.svg",
+  }),
+}
+</script>

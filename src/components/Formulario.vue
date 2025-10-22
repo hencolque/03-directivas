7ab8727e31c0360c8cdbb10a8ee189c3d8384921
Henry Colque Larrea <henry.colque@asfi.gov.bo>
2025-10-22 19:01:00 -0400

    ultimos cambios

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,18 +1,90 @@
 <template>
-  <form>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  <div class="row">
+    <div class="col-12 col-md-4">
+      <form @submit.prevent="registrarProyeto">
+        <div class="mb-3">
+          <label class="form-label">Proyecto</label>
+          <input v-model.trim="proyecto" type="text" class="form-control" required>
+        </div>
+
+        <div class="mb-3">
+          <label class="form-label">Actividad</label>
+          <select v-model="tipo" class="form-select" required>
+            <option disabled selected value="">Seleccione un tipo...</option>
+            <option>Aplicación Web con Vue.js</option>
+            <option>Backend Service con Node.js</option>
+            <option>App Móvil con React Native</option>
+          </select>
+        </div>
+
+        <div class="mb-3">
+          <label class="form-check-label">Urgente</label>
+          <input v-model="urgente" type="checkbox" class="form-check-input">
+        </div>
+        <button type="submit" class="btn btn-primary">Guardar</button>
+      </form>
+    </div>
+    <div class="col-12 col-md-8">
+        <h3>
+          Total Proyectos: {{ numeroProyectos }}
+        </h3>
+        <div class="table-responsive">
+          <table class="table table-dark table-hover">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Proyecto</th>
+                <th>Tipo</th>
+                <th>Urgente</th>
+                <th>Completado</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(proyecto, index) in proyectos" :key="index">
+                <td>{{ index + 1 }}</td>
+                <td>{{ proyecto.proyecto }}</td>
+                <td>{{ proyecto.tipo }}</td>
+                <td @click="cambiarEstado(proyecto, 'urgente')" :class="proyecto.urgente ? 'bg-success' : 'bg-danger'">{{ proyecto.urgente ? "SI" : "NO" }}</td>
+                <td @click="cambiarEstado(proyecto, 'completado')" :class="proyecto.completado ? 'bg-success' : 'bg-danger'">{{ proyecto.completado ? "Completo" : "Incompleto" }}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+    </div>
   </div>
-  <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
-  </div>
-  <div class="mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    proyecto: "",
+    tipo: "",
+    urgente: false,
+    proyectos: [],
+  }),
+  methods: {
+    registrarProyeto() {
+      const proyecto = {
+        proyecto: this.proyecto,
+        tipo: this.tipo,
+        urgente: this.urgente,
+        completado: false
+      };
+
+      this.proyectos.push(proyecto);
+
+      this.tipo = "";
+      this.proyecto = "";
+      this.urgente = false;
+    },
+    cambiarEstado(proyecto, campo) {
+      proyecto[campo] = !proyecto[campo];
+    },
+  },
+  computed: {
+    numeroProyectos() {
+      return this.proyectos.length;
+    }
+  }
+}
+</script>
